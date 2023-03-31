807de3ac3c8a91145d3a7cb22b0eeba07e09066d
Baadal VM <baadalvm@yahoo.com>
2023-03-31 16:58:41 +0530

    commit

--- flask_blog/init_db.py
+++ flask_blog/init_db.py
@@ -1,57 +1,95 @@
-import sqlite3
+# # # import mysql
+# # # # import pandas as pd
+# # # # import numpy as np
+# import csv
 
-connection = sqlite3.connect('database.db')
+# # # connection = mysql.connect('database.db')
+
+# from flask import Flask, render_template, request
+# from flask_mysqldb import MySQL
+# app = Flask(__name__)
 
 
-with open('schema.sql') as f:
-    connection.executescript(f.read())
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'kaipoche'
+# app.config['MYSQL_DB'] = 'test'
 
-cur = connection.cursor()
-input=pd.read_csv()
-cur.execute("INSERT INTO Tags (id,tags) VALUES (?, ?)", ('1', 'Python') )
+# mysql = MySQL(app)
 
-cur.execute("INSERT INTO Tags (id, tags) VALUES (?, ?)",
-            ('2', 'avascript')
-            )
-cur.execute("INSERT INTO Tags (id,tags) VALUES (?, ?)",
-            ('3', 'ython')
-            )
 
-cur.execute("INSERT INTO Tags (id, tags) VALUES (?, ?)",
-            ('4', 'java')
-            )
-cur.execute("INSERT INTO Tags (id,tags) VALUES (?, ?)",
-            ('5', 'Py')
-            )
+# # # with open('schema.sql') as f:
+# #     # mysql.connection.execute(f.read())
+# # cur = mysql.connection.cursor()
+# cur = mysql.connection.cursor()
+# # input = pd.read_csv("/home/baadalvm/repos/tags.csv")
+# # X = input[input.columns[0:]]
+# # X = np.array(X)
+# with open("/home/baadalvm/repos/tags.csv", 'r') as file:
+#   csvreader = csv.reader(file)
+#   for row in csvreader:
+#     cur.execute("INSERT INTO Tag (id,tags) VALUES (?, ?)", (row[0], row[1]) )
+# mysql.connection.commit()
+# mysql.connection.close()
+# # cur.execute("INSERT INTO Tags (id, tags) VALUES (?, ?)",
+# #             ('2', 'avascript')
+# #             )
+# # cur.execute("INSERT INTO Tags (id,tags) VALUES (?, ?)",
+# #             ('3', 'ython')
+# #             )
 
-cur.execute("INSERT INTO Tags (id, tags) VALUES (?, ?)",
-            ('6', 'script')
-            )
+# # cur.execute("INSERT INTO Tags (id, tags) VALUES (?, ?)",
+# #             ('4', 'java')
+# #             )
+# # cur.execute("INSERT INTO Tags (id,tags) VALUES (?, ?)",
+# #             ('5', 'Py')
+# #             )
 
-cur.execute("INSERT INTO Tags (id,tags) VALUES (?, ?)",
-            ('7', 'Python')
-            )
+# # cur.execute("INSERT INTO Tags (id, tags) VALUES (?, ?)",
+# #             ('6', 'script')
+# #             )
 
-cur.execute("INSERT INTO Tags (id, tags) VALUES (?, ?)",
-            ('8', 'avascript')
-            )
-cur.execute("INSERT INTO Tags (id,tags) VALUES (?, ?)",
-            ('9', 'ython')
-            )
+# # cur.execute("INSERT INTO Tags (id,tags) VALUES (?, ?)",
+# #             ('7', 'Python')
+# #             )
 
-cur.execute("INSERT INTO Tags (id, tags) VALUES (?, ?)",
-            ('10', 'java')
-            )
-cur.execute("INSERT INTO Tags (id,tags) VALUES (?, ?)",
-            ('11', 'Py')
-            )
+# # cur.execute("INSERT INTO Tags (id, tags) VALUES (?, ?)",
+# #             ('8', 'avascript')
+# #             )
+# # cur.execute("INSERT INTO Tags (id,tags) VALUES (?, ?)",
+# #             ('9', 'ython')
+# #             )
 
-cur.execute("INSERT INTO Tags (id, tags) VALUES (?, ?)",
-            ('12', 'script')
-           )
-cur.execute("INSERT INTO Tags (id, tags) VALUES (?, ?)",
-            ('13', 'script')
-           )
-connection.commit()
-connection.close()
+# # cur.execute("INSERT INTO Tags (id, tags) VALUES (?, ?)",
+# #             ('10', 'java')
+# #             )
+# # cur.execute("INSERT INTO Tags (id,tags) VALUES (?, ?)",
+# #             ('11', 'Py')
+# #             )
 
+# # cur.execute("INSERT INTO Tags (id, tags) VALUES (?, ?)",
+# #             ('12', 'script')
+# #            )
+# # cur.execute("INSERT INTO Tags (id, tags) VALUES (?, ?)",
+# #             ('13', 'script')
+# #            )
+
+
+import csv
+import MySQLdb
+
+mydb = MySQLdb.connect(host='localhost',
+    user='root',
+    passwd='kaipoche',
+    db='test')
+cursor = mydb.cursor()
+
+with open("/home/baadalvm/repos/tags.csv", 'r') as file:
+  csvreader = list(csv.reader(file))
+  for row in csvreader[1:]:
+    print(row)
+    cursor.execute('INSERT INTO Tag (id, tags ) VALUES("%s", "%s")',row)
+#close the connection to the database.
+mydb.commit()
+cursor.close()
+print ("Done")
