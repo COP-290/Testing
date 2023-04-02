1b700df2a602039080c28b6373301965be686e61
Baadal VM <baadalvm@yahoo.com>
2023-04-02 16:17:52 +0530

    Zaurez for sql

---
+++ 1.py
@@ -0,0 +1,29 @@
+# from _future_ import print_function
+import csv
+import MySQLdb
+
+print("Enter  File  To Be Export")
+# conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="", db="database")
+mydb = MySQLdb.connect(host='localhost',
+    user='root',
+    passwd='kaipoche',
+    db='test')
+cursor = mydb.cursor()
+# cursor = conn.cursor()
+#sql = 'CREATE DATABASE test1'
+# sql ='''DROP TABLE IF EXISTS `test1`; CREATE TABLE test1 (policyID int, statecode varchar(255), county varchar(255))'''
+sql = 'ALTER TABLE Question MODIFY COLUMN id INT AUTO_INCREMENT'
+# sql = 'ALTER TABLE Question MODIFY COLUMN id INT PRIMARY KEY'
+cursor.execute(sql)
+
+# with open('C:/Users/Desktop/Code/python/sample.csv') as csvfile:
+#     reader = csv.DictReader(csvfile, delimiter = ',')
+#     for row in reader:
+#         print(row['policyID'], row['statecode'], row['county'])
+#         # insert
+#         conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="", db="database")
+#         sql_statement = "INSERT INTO test1(policyID ,statecode,county) VALUES (%s,%s,%s)"
+#         cur = conn.cursor()
+#         cur.executemany(sql_statement,[(row['policyID'], row['statecode'], row['county'])])
+#         conn.escape_string(sql_statement)
+# mydb.commit()
