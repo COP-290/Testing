153fafce0b970f72739dd164a603c4a339e28775
Baadal VM <baadalvm@yahoo.com>
2023-04-04 10:48:44 +0530

    conn close after every method

--- flask_blog/answer.py
+++ flask_blog/answer.py
@@ -2,17 +2,23 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 from flask_paginate import Pagination, get_page_args
 import MySQLdb
-app=Flask(__name__)
 
-mydb = MySQLdb.connect(host='localhost',
+def requestConnection():
+    mydb = MySQLdb.connect(host='localhost',
     user='root',
     passwd='root',
     db='test')
-cursor = mydb.cursor()
+    return mydb
+
+def requestCursor(conn):
+    return conn.cursor()
+    # return conn.cursor(db.cursors.DictCursor)
+app=Flask(__name__)
 
 def get_id_question(tag): # output list of id from a given tag
-    ans=[]
     s=tag
+    conn=requestConnection()
+    cursor=requestCursor(conn)
     l=cursor.execute('SELECT * FROM Tag')
     l=cursor.fetchall()
     tag_list=[]
@@ -22,28 +28,40 @@ def get_id_question(tag): # output list of id from a given tag
         c=a[1:b-1]
         if s==c:
          tag_list.append(l[k][1])
+    cursor.close()
+    conn.close()
     return tag_list
 
 def question_from_id(id): # output list of all questions from a given id
+    conn=requestConnection()
+    cursor=requestCursor(conn)
     l=cursor.execute('SELECT * FROM Question where id = ' + str(id))
     l=cursor.fetchall()
     M = []
     for i in l:
         M.append(i)
+    cursor.close()
+    conn.close()
     return list(M)
 # print(question_from_id(80))
-# print(question_from_id("'flex'"))
+
 
 def questionTag_from_id(id): # list of tag from question id
+    conn=requestConnection()
+    cursor=requestCursor(conn)
     l=cursor.execute('SELECT tags FROM Tag where id = ' + str(id))
     l=cursor.fetchall()
     tag_list=[]
     # print(l)
     for k in range(0,len(l)):
         tag_list.append(l[k][0])
+    cursor.close()
+    conn.close()
     return tag_list
 
-def question_from_tag(tag):
+# print(questionTag_from_id(80))
+
+def question_from_tag(tag): # Time complexity is O(n2) think of modifying it.
     a=get_id_question(tag) # list of id for particular tag
     l=[]
     for i in a: 
@@ -53,6 +71,9 @@ def question_from_tag(tag):
             b.append(c)
             l.append(b)
     return l
+
+# print(question_from_tag('flex'))
+
 def question_per_page(offset=0,per_page=3,tag='flex'):
     l=question_from_tag(tag)
     n=len(l)
@@ -60,19 +81,16 @@ def question_per_page(offset=0,per_page=3,tag='flex'):
      post=l[offset:offset+per_page]
     else:
         post=l[offset:]
-    return (post,n)
+    return (post,min(n,offset+per_page))
 
 # a=(question_per_page(0,3,'flex'))
-# print(len(a[0]))
+# print(a[0],a[1])
 # # print(questionTag_from_id(80))
 @app.route('/')
 def index():
-    page, per_page, offset = get_page_args(page_parameter='page',per_page_parameter='per_page')
-    l=question_per_page(offset=offset,per_page=per_page,tag='flex')
-    n=(l[1])
-    l=l[0]
-    pagination = Pagination(page=1, per_page=3, total=n,css_framework='bootstrap5')
-    return render_template('ask_question.html',l=l,n=n,page=1,per_page=3,pagination=pagination)
+    l=question_from_tag('flex')
+    n=len(l)
+    return render_template('ask_question.html',l=l,n=n)
 
 @app.route('/<string:tag>/question',methods=['GET'])
 def question_page(tag): # took care when question is less than 3
