fe8c778543a08a2cf7e6c7ab5e7ce2223917e96f
Baadal VM <baadalvm@yahoo.com>
2023-04-05 15:21:04 +0530

    not changed

--- flask_blog/particular_question.py
+++ flask_blog/particular_question.py
@@ -39,16 +39,40 @@ def questionTag_from_id(id): # list of tag from question id
     conn.close()
     return tag_list
 
-def answer_from_id(id):
+def particular_que_from_id(id):
     b=question_from_id(id)
     tag=questionTag_from_id(id)
-    return b.append(tag)
-print(answer_from_id())
+    ans=[]
+    ans.append(b)
+    ans.append(tag)
 
+    return ans
+# print(answer_from_id())
+# print(particular_que_from_id(80))
+
+def answer_from_parent_id(id):
+    conn = requestConnection()
+    cursor = requestCursor(conn)
+    l = cursor.execute('SELECT * FROM Answer where Parent_ID = ' + str(id))
+    l = cursor.fetchall()
+    Answer_list = []
+    for k in range(len(l)):
+        Answer_list.append(l[k])
+    return Answer_list
+
+# print(answer_from_parent_id(80))
 @app.route('/')
 def particular_question():
-    l=question_from_id(80)
-    return render_template('answer.html',l=l,n=1)
+    conn = requestConnection()
+    cursor = requestCursor(conn)
+    l=cursor.execute(
+        "SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate FROM Orders INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;")
+    l=particular_que_from_id(80)
+    n=1
+    ans_list=answer_from_parent_id(80)
+    # print(ans_list)
+    m=len(ans_list)
+    return render_template('particular_question.html',l=l,n=n,ans_list=ans_list,m=m)
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0',debug=True,port=7000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True,port=7015)
