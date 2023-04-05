5bced276fa3ebe6b210f3cf71ecdbb92da90019e
Baadal VM <baadalvm@yahoo.com>
2023-04-06 01:36:40 +0530

    Jay/ .js files for login and sign up are added and also the .css file in App.css

---
+++ frontend/src/components/tag/login.js
@@ -0,0 +1,56 @@
+export default function Login() {
+    return (
+        <>
+        <body>
+
+<div class="row">
+  <div class="page_title col-12 d-flex justify-content-center">
+    -: Login :-
+  </div>
+</div>
+
+<div class="row">
+    <div class="col-12 d-flex justify-content-center mt-5">
+        <div class="sign_up_box p-3">
+            <form>
+                <div class="mb-3 row d-flex flex-column">
+                    <label for="staticUsername" class="col-sm-2 col-form-label label_title">Username</label>
+                    <div class="col-sm-10">
+                      <input type="text" readonly class="form-control-plaintext" id="staticUsername" value="username"></input>
+                    </div>
+                </div>
+                <div class="mb-3 row d-flex flex-column">
+                    <label for="staticEmail" class="col-sm-2 col-form-label label_title">Email</label>
+                    <div class="col-sm-10">
+                      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com"></input>
+                    </div>
+                </div>
+                <div class="mb-3 row d-flex flex-column">
+                    <label for="inputPassword" class="col-sm-2 col-form-label label_title">Password</label>
+                    <div class="col-12">
+                      <input type="password" class="form-control" id="inputPassword"></input>
+                    </div>
+                  </div>
+                <div class="mb-3 form-check">
+                  <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+                  <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <div class="mb-3 d-flex justify-content-center">
+                  Are you new here?
+                  <div class="px-1">
+                    <a href="signup.html" class="link-primary">click here</a>
+                  </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button type="submit" class="sign_up_btn btn btn-primary">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    
+</div>
+
+</body>
+        </>
+);
+}
---
+++ frontend/src/components/tag/sign_up.js
@@ -0,0 +1,53 @@
+export default function Signup() {
+    return (
+        <>
+        <body>
+
+
+
+<div class="row">
+  <div class="page_title col-12 d-flex justify-content-center">
+    -: Sign up :-
+  </div>
+</div>
+
+<div class="row">
+    <div class="col-12 d-flex justify-content-center mt-5">
+        <div class="sign_up_box p-3">
+            <form>
+                <div class="mb-3">
+                    <label for="exampleInputusername1" class="form-label label_title">Username</label>
+                    <input type="username" class="form-control" id="exampleInputusername1" aria-describedby="usernameHelp"></input>
+                  </div>
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label label_title">Email address</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="name@example.com" aria-describedby="emailHelp"></input>
+                  <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label label_title">Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1"></input>
+                </div>
+                <div class="mb-3 form-check">
+                  <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+                  <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <div class="mb-3 d-flex justify-content-center">
+                  Already have an account?
+                  <div class="px-1">
+                    <a href="login.html" class="link-primary">click here</a>
+                  </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button type="submit" class="sign_up_btn btn btn-primary">Sign up</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    
+</div>
+
+</body>
+        </>
+);
+}
