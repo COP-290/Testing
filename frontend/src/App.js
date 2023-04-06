84745a39c8d9f3f2891296c5cf7892cd8cfcc3aa
Baadal VM <baadalvm@yahoo.com>
2023-04-06 11:47:59 +0530

    Jay/ intiaation of the index.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Tag1 from './components/tag/tag1';
 import Tag from './components/tag/tag';
 import Navbar from './components/tag/nav';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -7,11 +8,27 @@ import Par_ques from './components/tag/particular_question';
 import New_ques from './components/tag/new_question';
 import Test from './components/tag/test';
 import Question from './components/tag/question';
+import Profile from './components/tag/profile';
+import Login from './components/tag/login';
+import Signup from './components/tag/sign_up';
+import Index from './components/tag';
 
 function App() {
   return (
     <>
         <Navbar/>
+
+        <Router>
+          <Routes>
+            <Route path="/tag1" element={<Tag1/>} />        
+          </Routes>
+        </Router>
+
+        <Router>
+          <Routes>
+            <Route path="/index" element={<Index/>} />        
+          </Routes>
+        </Router>
 
         <Router>
           <Routes>
@@ -43,6 +60,23 @@ function App() {
           </Routes>
         </Router>
 
+        <Router>
+          <Routes>
+            <Route path="/profile" element={<Profile/>} />        
+          </Routes>
+        </Router>
+
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login/>} />        
+          </Routes>
+        </Router>
+
+        <Router>
+          <Routes>
+            <Route path="/signup" element={<Signup/>} />        
+          </Routes>
+        </Router>
 
 </>
   );
