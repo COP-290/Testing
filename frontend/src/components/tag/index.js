5c901a6148c39bb36ecdc5d436d526de1659eb3c
Baadal VM <baadalvm@yahoo.com>
2023-04-06 12:07:36 +0530

    Jay/index.js added

--- frontend/src/components/tag/index.js
+++ frontend/src/components/tag/index.js
@@ -1,7 +1,28 @@
 export default function Index() {
     return (
         <>
+        <body>
+        <div class="row d-flex justify-content-center mt-3 column-gap-xl-3 column-gap-lg-2 row-gap-3">
+    <div class="sign_up_box p-3">
+      Not able to find the solution for your problem?
+    </div>
+    <div class="sign_up_box p-3">
+      Is your time being wasted on other websitesfor solutions?
+    </div>
+  </div>
 
+  <div class="row d-flex justify-content-center mt-3">
+    <div class="sign_up_box p-3">
+      To cope with these type of problems here we come with <a href="index.html" class="link-primary">askQ.com</a>.
+    </div>
+  </div>
+    
+  <div class="row d-flex justify-content-center mt-3">
+    <div class="sign_up_box p-3">
+      " The Important thing is not to stop questioning. " - Albert Einstein
+    </div>
+  </div>
+        </body>
         </>
 );
 }
