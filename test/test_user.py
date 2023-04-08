ee1612903bf3e3e5debb000192a2885dceb280df
zaureza <110770093+zaureza@users.noreply.github.com>
2023-04-08 20:51:49 +0530

    Create test_user.py

---
+++ test/test_user.py
@@ -0,0 +1,12 @@
+import unittest
+import csv
+import MySQLdb
+import datetime
+
+from flaskblog.user import dis_user
+
+L = ('1/8/2006', (66, 'Satyam', 'Something needs to be written there', datetime.datetime(2006, 8, 1, 15, 45, 37), datetime.datetime(2023, 4, 8, 20, 0, 10), 'India', 378, 893, 98, 'https://static.vecteezy.com/system/resources/previews/005/544/718/original/profile-icon-design-free-vector.jpg', 'www.gooogle.com', '1234'))
+class Test_User(unittest.TestCase):
+        def test_dis_User(self):
+                x = dis_user(66)
+                self.assertEqual((L),x)
